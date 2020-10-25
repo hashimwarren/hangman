31f9a4469ba0c4cf9868484de8ca5145f999c5c5
Hashim Warren <hashimwarren@gmail.com>
2020-10-25 06:02:19 -0400

    Complete challenge 4

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -29,19 +29,29 @@ Hangman.prototype.getPuzzle = function () {
 }
 
 Hangman.prototype.makeGuess = function (guess) {
-    guess.toLowerCase()
-    const uniqueGuess = !this.guessedLetters.includes(guess)
-    const badGuess = !this.word.includes(guess)
-    if (uniqueGuess) {
-        this.guessedLetters.push(guess)
-    }
 
-    if (badGuess && uniqueGuess) {
-        this.guesses--
+    if (this.status === 'playing') {
+        guess.toLowerCase()
+        const uniqueGuess = !this.guessedLetters.includes(guess)
+        const badGuess = !this.word.includes(guess)
+        if (uniqueGuess) {
+            this.guessedLetters.push(guess)
+        }
+
+        if (badGuess && uniqueGuess) {
+            this.guesses--
+
+        }
+
+
 
     }
 
     this.changeStatus()
+    this.printStatus()
+
+
+
 
 
 }
@@ -51,28 +61,29 @@ Hangman.prototype.changeStatus = function () {
     const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
 
     if (this.guesses <= 0) {
-        this.status = 'failed 👎🏽'
+        this.status = 'failed'
 
     } else if (finished) {
-        this.status = 'finished 🎈'
+        this.status = 'finished'
 
     } else {
-        this.status = 'playing...'
+        this.status = 'playing'
     }
 
-    // if (!this.getPuzzle().includes("*")) {
-    //     this.status = 'finished 🎈'
+}
 
-    // } else if (this.guesses <= 0 && this.getPuzzle().includes("*")) {
-    //     this.status = 'failed 👎🏽'
-    // }
+Hangman.prototype.printStatus = function () {
 
+    if (this.status === 'playing') {
+        return `Guesses left: ${this.guesses}`
 
+    } else if (this.status === 'failed') {
+        return `Nice try! The word was ${this.word.join('')}`
 
+    } else {
+        return `Great work! You guessed the word`
 
-
-
-
+    }
 
 }
 
