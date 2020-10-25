0eaab566294d044714f6eb509d2cfbbe2e8e2c4c
Hashim Warren <hashimwarren@gmail.com>
2020-10-25 11:18:09 -0400

    Refactor Person to use class syntax

--- oop/person.js
+++ oop/person.js
@@ -1,48 +1,36 @@
-// **constructor function** is a function that can be used to created objects. 
-// Use the **new** operator to create a new object off od the constructor function. 
-// The **this** keyword refers to the function itself. It implcitly returns an object. Leave **return** off
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
 
-// Prototypical inheritance
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
 
-const Person = function (firstName, lastName, age, likes) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
 
-}
+        return bio
 
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
+    }
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
 
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`
-    })
-
-
-    return bio
-
-}
-
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
+    }
 
 }
 
-const me = new Person('Hashim', 'Warren', 40, ['teaching, biking'])
 
-me.getBio = function () {
-    return 'This is fake'
 
-}
-
-me.setName('Darique Warren')
-console.log(me.getBio())
-
-const person2 = new Person('Alexandra', 'Houston', 38)
+const person = new Person('Alexandra', 'Houston', 38, ['Teaching'])
+const person2 = new Person('Clancy', 'Martin', 55)
 
 
 
-console.log(person2.getBio()) // expected: "Alexandra is 38". Printed error: "Cannot read property 'forEach' of undefined"
+console.log(person.getBio()) //?
+console.log(person2.getBio()); //?
